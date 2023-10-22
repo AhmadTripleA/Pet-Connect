453069e325260dcced98375b0ed1320ee939a196
Ahmad Akta <ahmadtriplea@outlook.com>
2023-10-22 20:46:31 +0300

    architecture works now, login returns the body

---
+++ Controllers/userController.js
@@ -0,0 +1,66 @@
+const User = require('../Models/userModel');
+const bcrypt = require('bcrypt');
+
+    exports.register = async (req, res) => {
+        try {
+            const { name, email, password } = req.body;
+
+            const existingUser = await User.findOne({ email: email });
+
+            // check if user email already exists in database
+            if (existingUser) {
+                console.log("User exists already");
+                res.status(409).json(({ error: 'User Already Exists.' }));
+                return;
+            }
+
+            // hashes password
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            const user = new User({
+                name,
+                email,
+                password: hashedPassword
+            });
+
+            // add user to DB
+            await user.save();
+
+            console.log(`New User: ${name}, ${email}`);
+            res.status(201).json({ message: 'User Created Successfully!' })
+
+        } catch (err) {
+            console.error(err);
+            res.status(400).json({ error: err.message })
+        }
+    }
+
+    exports.login = async (req, res) => {
+        try {
+            const { email, password } = req.body;
+
+            const user = await User.findOne({ email: email });
+
+            // if this email doesn't exist in DB...
+            if (!user) {
+                console.log("User Doesn't Exist");
+
+                res.status(401).json({ error: 'User Doesnt Exist' });
+                return;
+            }
+
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            // return user as JSON
+            if(passwordMatch){
+                res.status(200).json(user);
+
+                console.log('User Returned to Sender');
+            }
+
+        } catch (err) {
+            console.error(err);
+
+            res.status(400).json({ error: err.message })
+        }
+    }
--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const session = require('express-session');
 const morgan = require('morgan');
+const bodyParser = require ('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
-
 // Connect to MongoDB (replace 'your-database-name' with your database name)
-mongoose.connect('mongodb://127.0.0.1:27017/petcare', {
+mongoose.connect('mongodb://127.0.0.1:27017/MRC', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-// Create a User model and schema
-const User = mongoose.model('User', new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-}));
-
 
 // Parse form data
 app.use(express.urlencoded({ extended: true }));
@@ -27,64 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 // Log all incoming requests in simple format 
 app.use(morgan('Method=:method Endpoint=:url Status=:status Time=:response-time ms - :date[web]'));
 
-// DEBUGGING // Method used to debug how to decode JSON files on mobile 
-app.post('/debug', async (req, res) => {
-  const ahmadUser = {
-    name: "ahmad akta",
-    email: "ahmadakta@outlook.com",
-    location: "Amman, Jordan",
-    Nationality: "Syrian",
-    Age: 23
-  }
-  res.status(200).json(ahmadUser);
-  console.log("Message Sent!");
-})
-
-// Set up sessions (requires the 'express-session' middleware)
-app.use(session({
-  secret: '1337xdotto',
-  resave: false,
-  saveUninitialized: true,
-}));
-
-// Render the register/login page
-app.get('/', (req, res) => {
-  res.render('login-register');
-});
-
-// Register a user
-app.post('/register', async (req, res) => {
-  console.log(`New User: ${req.body.name}, ${req.body.email}`);
-  const { name, email, password } = req.body;
-  try {
-    const user = new User({ name, email, password });
-    await user.save();
-    //res.redirect('/'); Redirect to the login/register page
-
-    res.status(200).send('success');
-
-  } catch (error) {
-    res.status(500).send('Error registering user');
-  }
-});
-
-// Login a user
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email, password });
-    if (user) {
-      // Store the user ID in the session
-      req.session.userId = user.id;
-      res.redirect('/dashboard'); // Redirect to a dashboard or profile page
-      res.status(200).send('success');
-    } else {
-      res.status(401).send('Invalid login credentials');
-    }
-  } catch (error) {
-    res.status(500).send('Error logging in');
-  }
-});
+app.use('/users', require('./Routers/userRouter'));
 
 // Start the server
 app.listen(port, () => {
