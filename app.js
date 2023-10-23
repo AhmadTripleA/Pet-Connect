1efd706c5daebb997b19ddf38796b6e9b67bde1a
Ahmad Akta <ahmadtriplea@outlook.com>
2023-10-23 17:46:09 +0300

    implemented "incorrect password"

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
